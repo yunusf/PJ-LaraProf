5b0ac9362025373642902ee535fa83ca06183d2b
yunusf <yunusf42@gmail.com>
2023-02-21 13:48:20 +0700

    update

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,152 +1,27 @@
-    <footer class="page-footer font-small blue">
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-          integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-          crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
-          crossorigin="anonymous"></script>
+<div class="container-fluid text-center pt-5 pb-4 font-monospace" style="background: #434242">
+    <div class="row justify-content-md-center">
+        <div class="col" style="color: #f9f9f9">
+            <h3>Contact</h3>
+        </div>
+    <div class="row mt-4 justify-content-md-center" style="text-align: center; color: #ffffff">
+        <div class="col-4"></div>
+        <div class="col">
+            <i class="fa-brands fa-linkedin fa-2x"></i>
+            <a href="http://bit.ly/3IFqP9d" class="nav-link px-4" style="color:#ffffff">
+                <h6>LinkedIn</h6>
+            </a>
+        </div>
+        <div class="col">
+            <i class="fa-brands fa-github fa-2x"></i>
+            <a href="http://bit.ly/3xJYSGV" class="nav-link px-4" style="color:#ffffff">
+                <h6>GitHub</h6>
+            </a>
+        </div>
+        <div class="col-4"></div>
+    </div>
+</div>
 
-      <style>
-          @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap");
-          @import url("https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;500;600;700;800;900&display=swap");
+{{-- font aws --}}
+<script src="https://kit.fontawesome.com/aeab402783.js" crossorigin="anonymous"></script>
 
-          * {
-              font-family: 'Inter', sans-serif !important;
-          }
-
-          body .font-red-hat-display {
-              font-family: 'Red Hat Display', sans-serif !important;
-          }
-
-          body footer {
-              background: #313E60;
-              padding-top: 50px;
-              padding-bottom: 70px;
-          }
-
-          body footer .logo {
-              font-family: 'Red Hat Display', sans-serif;
-              font-weight: 700;
-              font-size: 26px;
-              line-height: 38px;
-              color: #FAFAFD;
-          }
-
-          body footer .social-media {
-              margin-top: 55px;
-          }
-
-          body footer .copyright {
-              font-family: 'Red Hat Display', sans-serif !important;
-              font-weight: 400;
-              font-size: 16px;
-              line-height: 135%;
-              color: #FAFAFD;
-              margin-top: 20px;
-          }
-
-          body footer .nav-footer p {
-              font-weight: 700 !important;
-              font-family: 'Red Hat Display', sans-serif !important;
-              font-size: 20px;
-              line-height: 135%;
-              color: #FAFAFD;
-          }
-
-          body footer .nav-footer a {
-              font-weight: 400 !important;
-              font-family: 'Red Hat Display', sans-serif !important;
-              font-size: 20px;
-              line-height: 135%;
-              color: #FAFAFD;
-          }
-
-          body footer li {
-              margin-bottom: 16px;
-          }
-      </style>
-      
-      <div class="container text-md-left">
-          <div class="row">
-              <div class="col-md-6 mt-md-0 mt-3 address">
-                  <div class="logo font-red-hat-display">
-                      GivMoney
-                  </div>
-                  <div class="social-media">
-                      <a href="#">
-                          <img src="https://api.elements.buildwithangga.com/storage/files/2/assets/Content/Content10/bi_linkedin.svg"
-                              alt="linkedin" class="img-fluid mr-4">
-                      </a>
-                      <a href="#">
-                          <img src="https://api.elements.buildwithangga.com/storage/files/2/assets/Content/Content10/bi_facebook.svg"
-                              alt="facebook" class="img-fluid mr-4">
-                      </a>
-                      <a href="#">
-                          <img src="https://api.elements.buildwithangga.com/storage/files/2/assets/Content/Content10/bi_twitter.svg"
-                              alt="twitter" class="img-fluid mr-4">
-                      </a>
-                      <a href="#">
-                          <img src="https://api.elements.buildwithangga.com/storage/files/2/assets/Content/Content10/bi_instagram.svg"
-                              alt="twitch" class="img-fluid mr-4">
-                      </a>
-                  </div>
-                  <div class="copyright font-red-hat-display">
-                      2021 All rights reserved.
-                  </div>
-              </div>
-              <hr class="clearfix w-100 d-md-none pb-3">
-              <div class="row col-md-6 nav-footer">
-                  <div class="col-md-4 mb-md-0 mb-3">
-                      <p>
-                          Features
-                      </p>
-                      <ul class="list-unstyled">
-                          <li>
-                              <a href="#!">Payments</a>
-                          </li>
-                          <li>
-                              <a href="#!">Collections</a>
-                          </li>
-                          <li>
-                              <a href="#!">Conversions</a>
-                          </li>
-                      </ul>
-                  </div>
-                  <div class="col-md-4 mb-md-0 mb-3">
-                      <p>
-                          Resources
-                      </p>
-                      <ul class="list-unstyled">
-                          <li>
-                              <a href="#!">Blog</a>
-                          </li>
-                          <li>
-                              <a href="#!">FAQ</a>
-                          </li>
-                          <li>
-                              <a href="#!">Partnerships</a>
-                          </li>
-                      </ul>
-                  </div>
-                  <div class="col-md-4 mb-md-0 mb-3">
-                      <p>
-                          Our Company
-                      </p>
-                      <ul class="list-unstyled">
-                          <li>
-                              <a href="#!">About Us</a>
-                          </li>
-                          <li>
-                              <a href="#!">Careers</a>
-                          </li>
-                          <li>
-                              <a href="#!">News & Media</a>
-                          </li>
-                      </ul>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title') | Landing Page</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
+
     <body>
         @include('includes.header')
             @yield('content')
         @include('includes.footer')
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     </body>
+
 </html>
 
 {{-- <!DOCTYPE html>
